8f514b0ff7afaa28268d444be0206de739fe6f16
venjiance <venjiance@98a03d04-1f35-11df-8982-67e453f37615>
2010-05-19 17:41:51 +0000

    worker_emporium.js - First implementation of emporium worker worker_+page.js - Add emporium to page worker.

--- _normal.user.js
+++ _normal.user.js
@@ -1378,7 +1378,8 @@ Page.defaults = {
 			army_viewarmy:			{url:'army_member.php', image:'view_army_on.gif'},
 			army_sentinvites:		{url:'army_reqs.php', image:'sent_invites_on.gif'},
 			army_newsfeed:			{url:'army_news_feed.php', selector:'#app'+APPID+'_army_feed_header'},
-                        apprentice_collect:             {url:'apprentice.php?collect=true', image:'ma_view_progress2.gif'}
+                        apprentice_collect:             {url:'apprentice.php?collect=true', image:'ma_view_progress2.gif'},
+                        emporium:                       {url:'goblin_emp.php', selector:'div[style*="emporium_top"]'}
 		}
 	}
 };
@@ -2834,6 +2835,121 @@ Elite.work = function(state) {
 	return false;
 };
 
+/********** Worker.Emporium **********
+* Get all ingredients and recipes
+*/
+var Emporium = new Worker('Emporium');
+
+Emporium.settings = {
+    advanced:true
+};
+
+Emporium.defaults = {
+	castle_age:{
+		pages:'emporium'
+	}
+};
+
+Emporium.data = {
+	ingredients:{},
+	prizes:{}
+};
+
+Emporium.option = {
+	perform:false,
+        min: 10
+};
+
+Emporium.runtime = {
+        choosen:null
+};
+
+Emporium.display = [
+	{
+		id:'perform',
+		label:'Automatically Perform',
+		checkbox:true
+	},{
+		id:'min',
+		label:'Min items per type',
+	        text:true,
+	        size:3
+	}
+];
+
+Emporium.parse = function(change) {
+	//debug(this.name,'Parser running.');
+	var ingredients = this.data.ingredients = {};
+        $('div.ingredientUnit').each(function(i,el){
+                // Skip chosen ingredients
+                if ($('img[src*="emporium_trade_in"]', el).length > 0) {
+                        var name = $('img', el).attr('src').filepart();
+                        ingredients[name] = $(el).text().regex(/x([0-9]+)/);
+                        //debug('Emporium','Found ingredient '+name+'('+ingredients[name]+')');
+		}
+	});
+	// Detect prizes
+	var prize_img=$('div[style*="emporium_pop_up"] div div img:not([src*="skip"])');
+	if(prize_img.length>0) {
+		var prize=prize_img.attr('src').filepart();
+		if(this.data.prizes[prize]) {
+			this.data.prizes[prize]++;
+		}
+		else {
+			this.data.prizes[prize]=0;
+		}
+		debug(this.name, 'Prize '+prize);
+	}
+};
+
+Emporium.update = function() {
+	var choosen = {}, ingredients=this.data.ingredients, i, n = 0;
+	//debug(this.name,'Updater running.');
+	for (i in ingredients) {
+		if (ingredients[i] > this.option.min) {
+			choosen[i]=ingredients[i]-this.option.min;
+			n += choosen[i];
+		}
+	}
+	if (n > 10) {
+		this.runtime.choosen = choosen;
+		//debug(this.name,'We have '+n+' ingredients to trade.');
+	}
+	else {
+		this.runtime.choosen = null;
+	}
+};
+
+Emporium.work = function(state) {
+	if (!this.option.perform || !this.runtime.choosen) {
+		return false;
+	}
+	//debug(this.name,'Worker running.');
+	if (!state || !Page.to('emporium')) {
+		return true;
+	}
+	var i,j,n=0;
+	for (i in this.runtime.choosen) {
+	        debug(this.name,'Selecting up to 10 '+i+' for trade.');
+		while (n<10 && this.runtime.choosen[i] > 0) {
+			if (!Page.click($('img[src*="emporium_trade_in"]', $('div.ingredientUnit img[src*="'+i+'"]').parent().parent()))) {
+				Page.reload(); // Can't find the ingredient we just parsed when coming here...
+			}
+			this.runtime.choosen[i]--;
+		    	n++;
+		}
+		if (n >= 10) {
+			debug(this.name,'Emporium full, going to roll.');
+			break;
+		}
+	}
+	if (n>=10 && Page.click($('img[src*="emporium_button.gif"]'))) {
+	        //debug(this.name,'Traded.');
+		return true;
+	}
+        //debug(this.name,'Finished.');
+	return false;
+};
 /********** Worker.Generals **********
 * Updates the list of Generals
 * Finds best General for other classes
